ec9ea6ba7948a4a7c9ccdc113dffd1e123ae91a9
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-03-23 16:51:29 -0300

    implemented TailwindCSS

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-const Index = (props) => {
+const Index = ({ repos, user }) => {
   return (
-    <div>
-      <h1>Home</h1>
-      <div>{props.currentDate}</div>
-      <pre>{props.repos.map(repo => {
+    <div className='container mx-auto'>
+      <h1 className='text-5xl font-bold font-mono my-2'>Helder Barbosa</h1>
+      <p className='text-lg font-bold'><a href={user.blog}>{user.blog}</a></p>
+      <p className='text-lg'>{user.location}</p>
+      <h3 className='text-2xl font-bold font-mono my-2'>Meus repositórios no GitHub :</h3>
+
+      <pre>{repos.map(repo => {
         return (
-          <div>
-            <p>---------------------------------------------</p>
-            <h3><a href={repo.html_url}>{repo.full_name}</a></h3>
+          <div key={repo.id} className=' border bg-gray-100 mx-4 my-4 p-4 hover:shadow-md'>
+            <h3 className='font-bold text-lg'><a href={repo.html_url}>{repo.full_name}</a></h3>
             <p>Language: {repo.language} / updated : {repo.updated_at.split('T', 1)}</p>
           </div>
         )
@@ -21,6 +23,9 @@ const Index = (props) => {
 export async function getServerSideProps(context) {
   const resRepos = await fetch('https://api.github.com/users/helder-barbosa/repos?sort=updated')
   const originalRepos = await resRepos.json()
+
+  const resUser = await fetch('https://api.github.com/users/helder-barbosa')
+  const user = await resUser.json()
 
   const dontShowRepos = ['']
 
@@ -42,7 +47,8 @@ export async function getServerSideProps(context) {
   return {
     props: {
       currentDate: new Date().toString(),
-      repos
+      repos,
+      user
 
     }
   }
