2caa28da0f946516261595f4d82bcaf8e700336c
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-04-09 21:46:12 -0300

    component: add footer

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full h-16 text-center bg-indigo-400 font-bold text-lg text-white mt-10'>
+      <p>Create by Helder Barbosa</p>
+      <p>2021</p>
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -3,57 +3,63 @@ import getUser from '../utils/getUser'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Education from '../components/Education'
+import Footer from '../components/Footer'
 
 const Index = ({ repos, user }) => {
   return (
-    <div id='header' className='container mx-auto'>
-      <Head>
-        <title>Helder Barbosa - FullStack Developer</title>
-        <link rel="icon" href="/static/favicon.ico" />
-      </Head>
+    <div>
+      <div id='header' className='container mx-auto'>
+        <Head>
+          <title>Helder Barbosa - FullStack Developer</title>
+          <link rel="icon" href="/static/favicon.ico" />
+        </Head>
 
-      <Header />
+        <Header />
 
-      <div>
-        <img className=" mx-auto w-64"
-          id="profile-photo"
-          src="/static/photo.png" alt="Helder Barbosa photograph" />
-      </div>
+        <div>
+          <img className=" mx-auto w-64"
+            id="profile-photo"
+            src="/static/photo.png" alt="Helder Barbosa photograph" />
+        </div>
 
-      <div className=" text-center">
-        <div className=" rounded-xl border bg-gray-100 mx-4 mb-4 p-4 hover:shadow-lg" >
-          <p className=' sm:text-xl text-base font-semibold p-3' >
-            FullStack Developer with knowledge in JavaScript, NodeJS and ReactJS.
-            Undergraduate in System Analysis and Development, concluding in 2021.
-            Hungry to Learn all the things that will improve my skills.
+        <div className=" text-center">
+          <div className=" rounded-xl border bg-gray-100 mx-4 mb-4 p-4 hover:shadow-lg" >
+            <p className=' sm:text-xl text-base font-semibold p-3' >
+              FullStack Developer with knowledge in JavaScript, NodeJS and ReactJS.
+              Undergraduate in System Analysis and Development, concluding in 2021.
+              Hungry to Learn all the things that will improve my skills.
 
           </p>
+          </div>
         </div>
-      </div>
 
-      <Education />
+        <Education />
 
-      <h3 className='text-2xl font-bold font-sans text-center my-2'>My repositories on GitHub :</h3>
+        <h3 className='text-2xl font-bold font-sans text-center my-2'>My repositories on GitHub :</h3>
 
-      <pre className=" block flex-wrap sm:grid grid-cols-3">{repos.map(repo => {
-        return (
-          <div
-            key={repo.id}
-            className=' font-sans rounded-xl border 
+        <pre className=" block flex-wrap sm:grid grid-cols-3">{repos.map(repo => {
+          return (
+            <div
+              key={repo.id}
+              className=' font-sans rounded-xl border 
             bg-gray-100 p-5 mx-2 my-2 sm:mx-3 my-3 
             p-4 hover:shadow-lg'>
-            <h3 className=' italic font-bold sm:text-lg flex-nowrap'><a href={repo.html_url}>{repo.full_name}</a></h3>
-            <ul>
-              <li>Language: {repo.language}</li>
-              <li>Updated : {repo.updated_at.split('T', 1)}</li>
-            </ul>
-          </div>
-        )
-      })}
-      </pre>
+              <h3 className=' italic font-bold sm:text-lg flex-nowrap'><a href={repo.html_url}>{repo.full_name}</a></h3>
+              <ul>
+                <li>Language: {repo.language}</li>
+                <li>Updated : {repo.updated_at.split('T', 1)}</li>
+              </ul>
+            </div>
+          )
+        })}
+        </pre>
+      </div>
+
+      <Footer />
     </div>
   )
 }
+export default Index
 
 export async function getServerSideProps(context) {
   const { repos, user } = await getUser('helder-barbosa')
@@ -67,4 +73,3 @@ export async function getServerSideProps(context) {
   }
 }
 
-export default Index
