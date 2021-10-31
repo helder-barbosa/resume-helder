8ded66271e3893ea6dea1678a544d223a2bad56e
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-10-30 21:33:54 -0300

    update Summary

--- components/Summary.js
+++ components/Summary.js
@@ -5,8 +5,7 @@ const Summary = () => {
     <div className=" text-center">
       <div className=" rounded-xl border bg-gray-100 mx-4 mb-4 p-4 hover:shadow-lg" >
         <p className=' sm:text-xl text-base font-semibold p-3' >
-          FullStack Developer with knowledge in JavaScript, NodeJS and ReactJS.
-          Undergraduate in System Analysis and Development, concluding in 2021.
+          Full-Stack Software Developer with strong background in NodeJS and ReactJS.
         </p>
       </div>
     </div>
